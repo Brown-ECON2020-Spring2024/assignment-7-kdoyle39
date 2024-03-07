7e3b1e7dfa6c8b2b10236e3b23e019007b132603
kdoyle39 <kaley_doyle@brown.edu>
2024-03-07 13:35:42 -0500

    final

--- tests/test-example.r
+++ tests/test-example.r
@@ -9,6 +9,9 @@ test_that("Section 3 test 1 and 2", {
   expect_equal(nobs(model), 1704)
 })
 
+install.packages("testdat")
+library(testdat)
+
 test_that("Section 3 test 3", {
   expect_unique(c("year", "country"), data = data)
   expect_range(gdpPercap, 0, 1e6, data = data)
